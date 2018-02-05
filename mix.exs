409fa4092732b66ac6e809f71df1271ce9786802
hosang <hosang@chain.partners>
2018-02-05 11:08:38 +0900

    Page 69 - 74

--- mix.exs
+++ mix.exs
@@ -40,7 +40,9 @@ defmodule Rumbl.Mixfile do
       {:phoenix_html, "~> 2.10"},
       {:phoenix_live_reload, "~> 1.0", only: :dev},
       {:gettext, "~> 0.11"},
-      {:cowboy, "~> 1.0"}
+      {:cowboy, "~> 1.0"},
+      {:comeonin, ">= 0.0.0"},
+      {:bcrypt_elixir, ">= 0.0.0"}
     ]
   end
 
