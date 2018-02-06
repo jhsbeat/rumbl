85024a835e83ccc0dc552d2ad42fa09b93211d88
hosang <hosang@chain.partners>
2018-02-06 17:47:39 +0900

    Page 129 - 136

--- config/test.exs
+++ config/test.exs
@@ -9,11 +9,4 @@ config :rumbl, RumblWeb.Endpoint,
 # Print only warnings and errors during test
 config :logger, level: :warn
 
-# Configure your database
-config :rumbl, Rumbl.Repo,
-  adapter: Ecto.Adapters.Postgres,
-  username: "postgres",
-  password: "postgres",
-  database: "rumbl_test",
-  hostname: "localhost",
-  pool: Ecto.Adapters.SQL.Sandbox
+import_config "test.secret.exs"
